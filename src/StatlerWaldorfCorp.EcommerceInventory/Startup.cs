4b9befb2003e6839954bf29902ff22e44a99f340
Chris Umbel <chris@chrisumbel.com>
2017-02-11 17:29:42 -0500

    mvc

--- src/StatlerWaldorfCorp.EcommerceInventory/Startup.cs
+++ src/StatlerWaldorfCorp.EcommerceInventory/Startup.cs
@@ -25,6 +25,7 @@ namespace StatlerWaldorfCorp.EcommerceInventory
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMvc();            
         }                       
     }
 }
