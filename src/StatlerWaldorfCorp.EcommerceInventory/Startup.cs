f7e20b934e14c7214b665f0871c75ff3cae088d3
Chris Umbel <chris@chrisumbel.com>
2017-02-11 19:40:14 -0500

    discoverable

--- src/StatlerWaldorfCorp.EcommerceInventory/Startup.cs
+++ src/StatlerWaldorfCorp.EcommerceInventory/Startup.cs
@@ -3,10 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using Steeltoe.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Linq;
 using StatlerWaldorfCorp.EcommerceInventory.Persistence;
+using Steeltoe.Discovery.Client;
 
 namespace StatlerWaldorfCorp.EcommerceInventory
 {
@@ -14,17 +14,36 @@ namespace StatlerWaldorfCorp.EcommerceInventory
     {
         public IConfigurationRoot Configuration { get; }
         
-        public Startup(IHostingEnvironment env)
+        private ILogger logger;
+        private ILoggerFactory loggerFactory;
+
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            var builder = new ConfigurationBuilder()
+                // Remove AddJsonFile later
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory()) 
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();            
+
+
+            this.loggerFactory = loggerFactory;
+            this.loggerFactory.AddConsole(LogLevel.Information);
+            this.loggerFactory.AddDebug();
+
+            this.logger = this.loggerFactory.CreateLogger("Startup");            
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddDiscoveryClient(Configuration);            
             services.AddScoped<ISKUStatusRepository, MemorySKUStatusRepository>();                        
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseDiscoveryClient();            
             app.UseMvc();            
         }                       
     }
