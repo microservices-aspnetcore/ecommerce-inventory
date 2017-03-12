c021fb2670a71da742bdfddd5418a2b97f31451c
autodidaddict <alothien@gmail.com>
2017-03-12 16:38:01 -0400

    Adding fake statuses.

--- src/StatlerWaldorfCorp.EcommerceInventory/Persistence/MemorySKUStatusRepository.cs
+++ src/StatlerWaldorfCorp.EcommerceInventory/Persistence/MemorySKUStatusRepository.cs
@@ -9,9 +9,27 @@ namespace StatlerWaldorfCorp.EcommerceInventory.Persistence
         public MemorySKUStatusRepository()
         {
             this.statuses = new Dictionary<int, SKUStatus>();
+
+
+            statuses[123] = new SKUStatus
+            {
+                SKU = 123,
+                QtyAvail = 5,
+                QtyOnHand = 1,
+                QtyBackOrdered = 0,
+                QtyOnHold = 0
+            };
+            statuses[456] = new SKUStatus
+            {
+                SKU = 456,
+                QtyAvail = 30,
+                QtyOnHand = 20,
+                QtyBackOrdered = 10,
+                QtyOnHold = 0
+            };
         }
 
-        public SKUStatus Get(int sku) 
+        public SKUStatus Get(int sku)
         {
             return this.statuses[sku];
         }
